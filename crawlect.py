f4c0367fb068e56f8a9270fc1e7f47164af32934
Yves Guillo <yvesguillo@gmail.com>
2025-04-02 13:13:23 +0200

    Resolved #5 : Added Crawlect parameters variables for scan depth : -d --depth ... Replaced Crawlect parameters variables name: -d for 'digest' become -o for 'output' as -d fit better for 'depth'. Included Crawlect parameters in constructor and class. Added Crawlec parameters place holders for current backlog features.

--- crawlect.py
+++ crawlect.py
@@ -4,6 +4,19 @@
 import argparse
 from pathlib import Path
 from format import Format
+from math import inf
+
+# From [Codemia](https://codemia.io/knowledge-hub/path/parsing_boolean_values_with_argparse)
+class BooleanAction(argparse.Action):
+    """This method converts argpars argument string to a boolean (e.g.: "yes" => True)."""
+    def __call__(self, parser, namespace, values, option_string = None):
+        if values.lower() in ("yes", "y", "true", "t", "1"):
+            setattr(namespace, self.dest, True)
+        elif values.lower() in ("no", "n", "false", "f", "0"):
+            setattr(namespace, self.dest, False)
+        else:
+            raise argparse.ArgumentTypeError(f"Unsupported boolean value: {values}")
+
 class Crawlect:
     """Crawl a given path to list and describe all files on a single markdown file."""
 
@@ -11,16 +24,34 @@ class Crawlect:
     with open("languages.json","rt") as file:
         _languages = file.read()
 
-    def __init__(self, path = ".", dest = "description.md", igno = []):
+    def __init__(self, path = ".", output = "description.md", recur = True, depth = inf, excl_ext_li = [], excl_dir_li = [], excl_fil_li = [], excl_ext_wr = [], excl_dir_wr = [], excl_fil_wr = [], incl_ext_li = [], incl_dir_li = [], incl_fil_li = [], incl_ext_wr = [], incl_dir_wr = [], incl_fil_wr = [], xenv = True, tree = True):
         self.path = path
         self.paths = Path(path)
-        self.dest = dest
-        self.files = self.listFilesIn(paths = self.paths,recur=True)
+        self.output = output
+        self.recur = recur
+        self.depth = depth
+
+        self.excl_ext_li = excl_ext_li
+        self.excl_dir_li = excl_dir_li
+        self.excl_fil_li = excl_fil_li
+        self.excl_ext_wr = excl_ext_wr
+        self.excl_dir_wr = excl_dir_wr
+        self.excl_fil_wr = excl_fil_wr
+        self.incl_ext_li = incl_ext_li
+        self.incl_dir_li = incl_dir_li
+        self.incl_fil_li = incl_fil_li
+        self.incl_ext_wr = incl_ext_wr
+        self.incl_dir_wr = incl_dir_wr
+        self.incl_fil_wr = incl_fil_wr
+
+        self.xenv = xenv
+        self.tree = tree
+        self.files = self.listFilesIn(paths = self.paths, recur = self.recur)
         self.title = self.paths.name
         self.digest = ""
 
     def listFilesIn(self, paths = None, files = [], recur = False):
-        """Append all files in specified path as strings in a list and return it."""
+        """Append all paths from specified path as Path object in a list and return it."""
         if paths is None:
             paths = self.paths
         for path in paths.iterdir():
@@ -29,9 +60,6 @@ class Crawlect:
             elif path.is_dir() and recur is True:
                 self.listFilesIn(paths = path, files = files, recur = recur)
         return files
-
-
-
 
 if __name__ == "__main__":
     try:
@@ -43,104 +71,126 @@ if __name__ == "__main__":
         )
 
         parser.add_argument(
-            "-p", "--path", "--path-to-crawl",
+            "-p", "--path", "--path_to_crawl",
             type = str,
             default = ".",
-            help = "Path to crawl (default is current folder \".\")")
+            help = "Path to crawl (default is current folder \".\").")
 
         parser.add_argument(
-            "-d", "--digest", "--digest-output-file",
+            "-o", "--output", "--output_file",
             type = str,
             default = "description.md",
-            help = "Output markdown digest file.")
+            help = "Output markdown digest file (default is description.md).")
 
         parser.add_argument(
-            "-r", "--recur", "--recursive-crawling",
-            action = "store_true",
-            help = "Enable recursive crawling.")
+            "-r", "--recur", "--recursive_crawling",
+            type = str,
+            choices = ["yes", "no", "y", "n", "true", "false", "t", "f", "1", "0"],
+            action = BooleanAction,
+            default = "True",
+            help = "Enable recursive crawling (default is True).")
 
         parser.add_argument(
-            "-xel", "--excl-ext-li", "--excluded-extentions-from-listing",
+            "-d", "--depth", "--recursive_crawling_depth",
+            type = int,
+            default = inf,
+            help = "Scan depth limit (default is infinite).")
+
+        parser.add_argument(
+            "-xel", "--excl_ext_li", "--excluded_extentions_from_listing",
             nargs = "*",
             default = [],
-            help = "List of file extensions to exclude from listing (e.g., .jpg .png).")
+            help = "List of file extensions to exclude from listing (e.g.: .jpg, .png).")
 
         parser.add_argument(
-            "-xdl", "--excl-dir-li", "--excluded-directories-from-listing",
+            "-xdl", "--excl_dir_li", "--excluded_directories_from_listing",
             nargs = "*",
             default = [],
-            help = "List of directories to exclude from listing (e.g., bin render).")
+            help = "List of directories to exclude from listing (e.g.: bin, render).")
 
         parser.add_argument(
-            "-xfl", "--excl-fil-li", "--excluded-files-from-listing",
+            "-xfl", "--excl_fil_li", "--excluded_files_from_listing",
             nargs = "*",
             default = [],
-            help = "List of files to exclude from listing (e.g., README.md .png).")
+            help = "List of files to exclude from listing (e.g.: README.md, profile.png).")
 
         parser.add_argument(
-            "-xew", "--excl-ext-wr", "--excluded-extentions-from-writing",
+            "-xew", "--excl_ext_wr", "--excluded_extentions_from_writing",
             nargs = "*",
             default = [],
-            help = "List of file extensions to exclude from writing (e.g., .jpg .png).")
+            help = "List of file extensions to exclude from writing (e.g.: .jpg, .png).")
 
         parser.add_argument(
-            "-xdw", "--excl-dir-wr", "--excluded-directories-from-writing",
+            "-xdw", "--excl_dir_wr", "--excluded_directories_from_writing",
             nargs = "*",
             default = [],
-            help = "List of directories to exclude from writing (e.g., bin render).")
+            help = "List of directories to exclude from writing (e.g.: bin, render).")
 
         parser.add_argument(
-            "-xfw", "--excl-fil-wr", "--excluded-files-from-writing",
+            "-xfw", "--excl_fil_wr", "--excluded_files_from_writing",
             nargs = "*",
             default = [],
-            help = "List of files to exclude from writing (e.g., README.md .png).")
+            help = "List of files to exclude from writing (e.g.: README.md, profile.png).")
 
         parser.add_argument(
-            "-iel", "--incl-ext-li", "--include-extentions-from-listing",
+            "-iel", "--incl_ext_li", "--include_extentions_from_listing",
             nargs = "*",
             default = [],
-            help = "List of file extensions to include in listing (e.g., .jpg .png).")
+            help = "List of file extensions to include in listing (e.g.: .jpg, .png).")
 
         parser.add_argument(
-            "-idl", "--incl-dir-li", "--include-directories-from-listing",
+            "-idl", "--incl_dir_li", "--include_directories_from_listing",
             nargs = "*",
             default = [],
-            help = "List of directories to include in listing (e.g., bin render).")
+            help = "List of directories to include in listing (e.g.: bin, render).")
 
         parser.add_argument(
-            "-ifl", "--incl-fil-li", "--include-files-from-listing",
+            "-ifl", "--incl_fil_li", "--include_files_from_listing",
             nargs = "*",
             default = [],
-            help = "List of files to include in listing (e.g., README.md .png).")
+            help = "List of files to include in listing (e.g.: README.md, profile.png).")
 
         parser.add_argument(
-            "-iew", "--incl-ext-wr", "--include-extentions-from-writing",
+            "-iew", "--incl_ext_wr", "--include_extentions_from_writing",
             nargs = "*",
             default = [],
-            help = "List of file extensions to include for writing (e.g., .jpg .png).")
+            help = "List of file extensions to include for writing (e.g.: .jpg, .png).")
 
         parser.add_argument(
-            "-idw", "--incl-dir-wr", "--include-directories-from-writing",
+            "-idw", "--incl_dir_wr", "--include_directories_from_writing",
             nargs = "*",
             default = [],
-            help = "List of directories to include for writing (e.g., bin render).")
+            help = "List of directories to include for writing (e.g.: bin, render).")
 
         parser.add_argument(
-            "-ifw", "--incl-fil-wr", "--include-files-from-writing",
+            "-ifw", "--incl_fil_wr", "--include_files_from_writing",
             nargs = "*",
             default = [],
-            help = "List of files to include for writing (e.g., README.md .png).")
+            help = "List of files to include for writing (e.g.: README.md, profile.png).")
+
+        parser.add_argument(
+            "-xen", "--xenv", "--randomize_env_variables",
+            type = str,
+            choices = ["yes", "no", "y", "n", "true", "false", "t", "f", "1", "0"],
+            action = BooleanAction,
+            default = "True",
+            help = "Randomize .env variables to mitigate sensitive info leak risk (default is True).")
+
+        parser.add_argument(
+            "-tre", "--tree", "--visualize_directory_tree",
+            type = str,
+            choices = ["yes", "no", "y", "n", "true", "false", "t", "f", "1", "0"],
+            action = BooleanAction,
+            default = "True",
+            help = "Visualize directory tree in the output file (default is True).")
 
         args = parser.parse_args()
 
-        # from inspect import getmembers
-        # from pprint import pprint
-        # pprint(getmembers(args))
-    
-        toto = Crawlect(args.path)
-        
+        toto = Crawlect(path = args.path, output = args.output, recur = args.recur, depth = args.depth, excl_ext_li = args.excl_ext_li, excl_dir_li = args.excl_dir_li, excl_fil_li = args.excl_fil_li, excl_ext_wr = args.excl_ext_wr, excl_dir_wr = args.excl_dir_wr, excl_fil_wr = args.excl_fil_wr, incl_ext_li = args.incl_ext_li, incl_dir_li = args.incl_dir_li, incl_fil_li = args.incl_fil_li, incl_ext_wr = args.incl_ext_wr, incl_dir_wr = args.incl_dir_wr, incl_fil_wr = args.incl_fil_wr, xenv = args.xenv, tree = args.tree)
 
-        
+        # for file in toto.files:
+        #     print(file)
+
         a = Format(toto)
         print("pass")
         a.typeCodeBox()
