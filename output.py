b0b70653b1ce1a036afc80a2665317bb1bcce941
Yves Guillo <yvesguillo@gmail.com>
2025-04-05 16:46:17 +0200

    Initialize #10: *Output* class.

--- output.py
+++ output.py
@@ -2,15 +2,43 @@
 """outpu.py contains Crawlect output utilities."""
 
 from pathlib import Path
+from datetime import datetime
+
+class Output:
+    """Output class provide standard Crawlec output features."""
+    def __init__(self, name = None, prefix = "output", suffix = ".md"):
+        self.name = name
+        self.prefix = prefix
+        self.suffix = suffix
+
+    def standardOutputName(cls):
+        if cls.name is None:
+            now = datetime.now()
+            return cls.prefix + "-" + str(now.year) + str("{:02d}".format(now.month)) + str("{:02d}".format(now.day)) + str("{:02d}".format(now.hour)) + str("{:02d}".format(now.minute)) + str("{:02d}".format(now.second)) + cls.suffix
+        else:
+            return cls.name
+
+    @classmethod
+    def compose(cls, path = None, composition = []):
+        """Append composition to output file."""
+        if path == None:
+            path = cls.standardOutputName()
+        pass
 
 try:
 
-    path = Path(".")
+    output = Output()
+    print(output.standardOutputName())
 
-    with path.open("test.md") as file:
-        file.write(f"## {path.name}")
-        file.write(f"{path}")
-        file.write("last line so far.")
+    # path = Path("test.md")
+    # print(path.exists())
+
+    # with path.open("a") as file:
+    #     file.write(f"## {path.name}\n")
+    #     file.write(f"{path}\n")
+    #     file.write("last line so far.\n")
+
+    # print(path.exists())
 
 except KeyboardInterrupt:
     print("Interupted by user.")
---
+++ test.md
@@ -0,0 +1,6 @@
+## test.mdtest.mdlast line so far.## test.md
+test.md
+last line so far.
+## test.md
+test.md
+last line so far.
