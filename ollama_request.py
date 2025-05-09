8f497ab294b20462908b5711f22c5e178dae0700
Yves Guillo <yvesguillo@gmail.com>
2025-05-09 22:07:47 +0200

    Restored LLM files.

---
+++ ollama_request.py
@@ -0,0 +1,64 @@
+#! /usr/bin/env python3
+
+from ollama import Client
+
+# Custom modules.
+from llm import LLM
+
+class Ollama_request(LLM):
+    """Extend LLM class for Ollama support."""
+
+    def __init__(self, host = None, model = None):
+
+        # Validate.
+        if host is None:
+            raise AttributeError(f"\n# Argument error #\n{type(self).__name__} requires an Ollama host URL (e.g.: \"http://localhost:11434\"), got {repr(host)}.")
+
+        if model is None:
+            raise AttributeError(f"\n# Argument error #\n{type(self).__name__} requires a pulled LLM model (e.g.: \"dolphin-phi\"), got {repr(model)}.")
+
+        super().__init__()
+
+        # Store the class arguments for __repr__.
+        self.args = {}
+
+        # Open AI settings.
+        self.host = host
+        self.args["host"] = self.host
+        self.model = model
+        self.args["model"] = self.model
+
+        self.client = Client(host = host)
+
+    def _prompt(self, message):
+        completion = self.client.chat(
+            model = self.model,
+            messages = [
+                {"role": "user", "content": message}
+            ]
+        )
+
+        return response['message']['content']
+
+
+####################
+# INTERACTIVE MODE #
+####################
+
+if __name__ == "__main__":
+
+    import traceback
+
+    try:
+        pass
+
+    except KeyboardInterrupt:
+        print("Interupted by user.")
+
+    except Exception as error:
+        print(f"\nUnexpected {type(error).__name__}:\n{error}\n")
+
+        # Debug.
+        lines = traceback.format_tb(error.__traceback__)
+        for line in lines:
+            print(line)
